bd9f1811cfa14007815950dc13d7cbf3000c81e6
tanya-06 <tanya.arora@agrevolution.in>
2022-07-22 18:10:44 +0530

    Fix config issue when dynamicaaly changing keycloak server

--- authsdk/src/main/java/com/example/dehaatauthsdk/Configuration.kt
+++ authsdk/src/main/java/com/example/dehaatauthsdk/Configuration.kt
@@ -210,9 +210,13 @@ class Configuration(private val mContext: Context, private val _clientId:String,
         private const val PREFS_NAME = "config"
         private const val KEY_LAST_HASH = "lastHash"
         private var sInstance = WeakReference<Configuration?>(null)
-        fun getInstance(context: Context, _clientId:String, isDebugMode:Boolean): Configuration {
+        fun getInstance(context: Context, _clientId:String, isDebugMode:Boolean,
+                        isLogin: Boolean): Configuration {
             var config = sInstance.get()
-            if (config == null) {
+            /* (isLogin && isDebugMode) This check is required when we want to change base url
+            ** for keycloak server
+             */
+            if (config == null || (isLogin && isDebugMode)) {
                 config = Configuration(context,_clientId,isDebugMode)
                 sInstance = WeakReference(config)
             }
